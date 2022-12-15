90d8aa65d4521152dca05f47c17bf9c57b2b8654
tomineodegard <tomi0105@stud.kea.dk>
2022-12-16 00:30:04 +0100

    svg chevron on filters

--- page-mode.php
+++ page-mode.php
@@ -23,7 +23,9 @@ get_header(); ?>
 
 				<div class="dropdown_menu_container">
 					<div class="dropdown">
-						<button class="filter_btn">Filtrer efter kategori</button>
+					<button class="filter_btn">Filtrer efter kategori <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/></svg>
+						</button>
 						<nav class="dropdown_content hide" id="dropdown_filter_options"><div class="filter chosen" data-taxonomy="all"></div></nav>
 					</div>
 				</div>
